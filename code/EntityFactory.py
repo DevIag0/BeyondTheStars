de903a8d36afd4a39c8f4bad922f864bab609f6f
DevIag0 <160984878+DevIag0@users.noreply.github.com>
2025-07-01 20:21:23 -0300

    implementado player1

--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -1,5 +1,6 @@
 from code.Background import Background
 from code.Const import ALTURA_JANELA, LARGURA_JANELA
+from code.Player import Player
 
 
 class EntityFactory:
@@ -10,7 +11,11 @@ class EntityFactory:
             case "Level1bg":
                 list_bg = []
                 for i in range(4):
-                    list_bg.append(Background(name=f'Level1bg{i}',position=(0, 0)))
+                    list_bg.append(Background(name=f'Level1bg{i}', position=(0, 0)))
                     list_bg.append(Background(name=f'Level1bg{i}', position=(0, -ALTURA_JANELA)))
 
                 return list_bg
+
+            case "Player":
+                return Player('Player1', position=(LARGURA_JANELA // 2, ALTURA_JANELA // 2 - 100))
+
