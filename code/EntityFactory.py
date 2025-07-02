3feda51d20b3aa388a680ba19fedbc175c4dadcb
DevIag0 <160984878+DevIag0@users.noreply.github.com>
2025-07-02 00:16:07 -0300

    tirando inimigo de fora da tela

--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -25,7 +25,7 @@ class EntityFactory:
                 return Player('Player2', position=(LARGURA_JANELA // 2 + 30, ALTURA_JANELA - 100))
 
             case "Enemy1":
-                return Enemy('Enemy1', position=(random.randint(0, LARGURA_JANELA - 10), -100))
+                return Enemy('Enemy1', position=(random.randint(30, LARGURA_JANELA - 30), -100))
 
             case "Enemy2":
-                return Enemy('Enemy2', position=(random.randint(0, LARGURA_JANELA - 10) , -100))
+                return Enemy('Enemy2', position=(random.randint(30, LARGURA_JANELA - 30), -100))
