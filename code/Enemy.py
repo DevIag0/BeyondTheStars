23f9627e495019819c1c28dd63133e2266d24e3f
DevIag0 <160984878+DevIag0@users.noreply.github.com>
2025-07-02 00:16:36 -0300

    implementando animacao diferente para 2 inimigos

--- code/Enemy.py
+++ code/Enemy.py
@@ -1,3 +1,4 @@
+import pygame
 from code.Const import ENTITY_SPEED, ALTURA_JANELA
 from code.Entity import Entity
 
@@ -5,6 +6,17 @@ from code.Entity import Entity
 class Enemy(Entity):
     def __init__(self, name: str, position: tuple):
         super().__init__(name, position)
+        self.frames = [
+            pygame.image.load(f"./asset/{name}_slow.png").convert_alpha(),
+            pygame.image.load(f"./asset/{name}.png").convert_alpha(),
+            pygame.image.load(f"./asset/{name}_boost.png").convert_alpha()
+        ]
+
+        # Configuração da animação
+        self.frame_index = 0
+        self.animation_timer = 0
+        self.animation_speed = 150  # milissegundos entre frames
+        self.last_update_time = pygame.time.get_ticks()
 
     # Lista para armazenar os frames da animação
 
@@ -13,3 +25,14 @@ class Enemy(Entity):
         if self.rect.top >= ALTURA_JANELA:
             self.rect.top = -ALTURA_JANELA
 
+        self.animacao_frente_atras()
+
+    def animacao_frente_atras(self, ):  # Lógica de animação
+        current_time = pygame.time.get_ticks()
+        if current_time - self.last_update_time > self.animation_speed:
+            self.last_update_time = current_time
+            # Avança para o próximo frame e volta ao início se chegou ao final
+            self.frame_index = (self.frame_index + 1) % len(self.frames)
+            # Atualiza a imagem atual para o frame correto
+            self.surf = self.frames[self.frame_index]
+            # Define a imagem do jogador como o frame atual
