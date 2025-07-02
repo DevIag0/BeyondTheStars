f167aec7517668288382b6765464e39ca917df63
DevIag0 <160984878+DevIag0@users.noreply.github.com>
2025-07-01 23:10:04 -0300

    fabricando enemy

--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -1,5 +1,7 @@
+import random
 from code.Background import Background
 from code.Const import ALTURA_JANELA, LARGURA_JANELA
+from code.Enemy import Enemy
 from code.Player import Player
 
 
@@ -22,3 +24,8 @@ class EntityFactory:
             case "Player2":
                 return Player('Player2', position=(LARGURA_JANELA // 2 + 30, ALTURA_JANELA - 100))
 
+            case "Enemy1":
+                return Enemy('Enemy1', position=(random.randint(0, LARGURA_JANELA - 10), -100))
+
+            case "Enemy2":
+                return Enemy('Enemy2', position=(random.randint(0, LARGURA_JANELA - 10) , -100))
